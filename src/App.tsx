d738edf25590ea27b3c9cd91ef2467213743b6b9
Patryk <patryk.r.nowak@gmail.com>
2023-07-27 07:45:40 +0100

    components and types continue

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
 const App: React.FC = () => {
   const todos = [{id: "t1", text: "Finish it"}]
 
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  }
 
   return (
     <div className="App">
+      <NewTodo onAddTodo={todoAddHandler}/>
      <TodoList items={todos}/> 
     </div>
   );
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,30 @@
+import React, {useRef} from 'react'
+
+// typically you would go for interface just for sake of demo
+
+
+type NewTodoProps = {
+    onAddTodo: (todoText: string) => void;
+}
+
+const NewTodo: React.FC<NewTodoProps> = props => {
+    const textInputRef = useRef<HTMLInputElement>(null);
+    const todoSubmitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+        const enteredText = textInputRef.current!.value;
+        props.onAddTodo(enteredText);
+
+    };
+
+
+    return <form onSubmit={todoSubmitHandler}>
+        <div>
+            <label htmlFor="todo-text">TODO TEXT</label>
+            <input type="text" id="todo-text" ref={textInputRef} />
+        </div>
+        <button type='submit'>ADD TODO</button>
+    </form>
+};
+
+
+export default NewTodo;
