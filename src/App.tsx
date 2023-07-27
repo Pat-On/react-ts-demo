8262d65c15a17def3f2aa67daadd2c44df1202f6
Patryk <patryk.r.nowak@gmail.com>
2023-07-27 07:54:43 +0100

    state with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
 
 const App: React.FC = () => {
-  const todos = [{id: "t1", text: "Finish it"}]
+  const [todos, setTodos] = useState<Todo[]>([{ id: "t1", text: "Finish it" }]);
 
   const todoAddHandler = (text: string) => {
     console.log(text);
+    setTodos(prevTodos => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text }
+    ])
   }
 
   return (
     <div className="App">
-      <NewTodo onAddTodo={todoAddHandler}/>
-     <TodoList items={todos}/> 
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} />
     </div>
   );
 }
